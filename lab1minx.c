ac8740bfc653d7cb6a384963d1604f3a4d99ac7b
6704062611191 <s6704062611191@email.kmutnb.ac.th>
2024-11-26 15:09:10 +0700

    ข้อที่ 2

--- lab1minx.c
+++ lab1minx.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 int main() {
-	int a = 150;
-	int b = 72;
-	if ( a > b ) {
-		printf( "a is greater than b" );
+	int n1;
+	int n2;
+	scanf("%d", &n1);
+	scanf("%d", &n2);
+	if ( n1 > n2 ) {
+		printf( "n1 is greater than n2" );
+		printf( "\n%d",(n1-n2));
 	}
 	else {
-		printf( "b is greater than a" );
-	}		
+		printf( "n2 is greater than n1" );
+		printf( "\n%d",(n2-n1));
+	}	
 }
 

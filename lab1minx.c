3cd0a4f42ccc000f46579f5a872cc830c2b301f3
Chanunchida Thipma <s6704062611191@email.kmutnb.ac.th>
2024-12-15 20:03:31 +0700

    ข้อ 4

--- lab1minx.c
+++ lab1minx.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main() {
 	int n1, n2;
-    scanf("%d %d", &n1, &n2);
+    scanf("%d", &n1);
+	scanf("%d", &n2);
 	if ( n1 > n2 ) {
 		printf( "n1 is greater than n2" );
         printf( "\n%d", (n1-n2) );
